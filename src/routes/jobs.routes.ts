2039d05741c327f78abf2141f1b7a4f983ab8d35
santanarscs <raphaelstn@gmail.com>
2021-06-30 16:42:49 -0300

    create routes for jobs

--- src/routes/jobs.routes.ts
+++ src/routes/jobs.routes.ts
@@ -1,54 +1,24 @@
-import axios from 'axios'
 import { Router, Request, Response } from 'express'
 import { SchedulesRepository } from '../modules/schedules/infra/typeorm/repositories/SchedulesRepository'
+import { JobsCongressRepository } from '../modules/schedules/infra/typeorm/repositories/JobsCongressRepository'
 import { ListSchedulesByRepeatService } from '../modules/schedules/services/ListSchedulesByRepeatService'
-import qs from 'qs'
+import { RunDailyJobCongressService } from '../modules/schedules/services/RunDailyJobCongressService'
+
 const jobsRoutes = Router()
 
-jobsRoutes.get('/', async (request: Request, response: Response) => {
+jobsRoutes.get('/daily', async (request: Request, response: Response) => {
   const schedulesRepository = new SchedulesRepository()
+  const jobsCongressRepository = new JobsCongressRepository()
+  
   const listScheduleByRepeatService = new ListSchedulesByRepeatService(schedulesRepository)
-
+  const runDailyJobService = new RunDailyJobCongressService(jobsCongressRepository)
+  
   const schedules = await listScheduleByRepeatService.execute('daily')
 
   const activeSchedules = schedules.filter(schedule => schedule.active === true)
-
-  const items = await Promise.all(
-    activeSchedules.map(async schedule => {
-      const {data} = await axios.get('https://dadosabertos.camara.leg.br/api/v2/proposicoes', {
-        params: {
-          keywords: schedule.tags,
-        },
-        paramsSerializer: (params) => {
-          return qs.stringify(params, {indices: false})
-        }
-      })
-      return {
-        ...schedule,
-        data
-      }
-    })
-  )
-
-  // const metadatas = activeSchedules.map(async schedule => {
-    
-  //   return {
-  //     ...schedule,
-  //     history: data
-  //   }
-  // })
-  // const { data } = await axios.get('https://dadosabertos.camara.leg.br/api/v2/proposicoes', {
-  //   params: {
-  //     dataInicio: '2021-05-01',
-  //     dataFim: '2021-05-31',
-  //     itens: 100,
-  //     keywords: ['mineração', 'defesa'],
-  //   },
-  //   paramsSerializer: (params) => {
-  //     return qs.stringify(params, {indices: false})
-  //   }
-  // })
-
+  
+  const items = await runDailyJobService.execute(activeSchedules)
+  
   return response.status(200).json(items)
 })
 
