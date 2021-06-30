50adbada67c4c88b115e2d5cf48ef624d91c0f6e
santanarscs <raphaelstn@gmail.com>
2021-06-30 16:34:02 -0300

    add in server middlware error

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata';
-import express from 'express'
-
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import AppError from './AppError';
 import { schedulesRoutes } from './routes/schedules.routes'
 import { jobsRoutes } from './routes/jobs.routes';
 
 import './database';
-// import './jobs/daily'
 const app = express()
 
 app.use(express.json())
@@ -13,5 +13,20 @@ app.use(express.json())
 app.use("/schedules", schedulesRoutes)
 
 app.use('/jobs', jobsRoutes)
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server is running'))
