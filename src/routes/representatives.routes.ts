39cfc620e6d59f3ff3f937a631f78a365d49b714
santanarscs <raphaelstn@gmail.com>
2021-07-14 08:13:05 -0300

    add routes representatives

--- src/routes/representatives.routes.ts
+++ src/routes/representatives.routes.ts
@@ -1,13 +1,27 @@
 import { ListRepresentativesService } from '../modules/schedules/services/ListRepresentativesService'
 import { Router, Request, Response } from 'express'
+import { DetailRepresentativeService } from '../modules/schedules/services/DetailRepresentativeService'
 
 const representativesRoutes = Router()
 
+representativesRoutes.get('/:id', async (request: Request, response: Response) => {
+  const { id } = request.params
+  const detailRepresentativeService = new DetailRepresentativeService()
+  const representative = await detailRepresentativeService.execute(id)
+
+  return response.status(200).json(representative)
+})
+
 representativesRoutes.get('/', async (request: Request, response: Response) => {
+  const { page, limit, name } = request.query
   const listRepresentativesService = new ListRepresentativesService()
-  const {representatives, totalCount} = await listRepresentativesService.execute(1)
+  const {representatives, totalCount} = await listRepresentativesService.execute({
+    page: Number(page),
+    limit: Number(limit),
+    name: name ? String(name) : undefined,
+  })
 
-
+  response.header('x-total-count', String(totalCount));
   return response.status(200).json(representatives)
 })
 
